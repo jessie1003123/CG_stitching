298b7a8164038a8c02508f14b66b6f7c335b1196
Jessie <80041170+jessie1003123@users.noreply.github.com>
2023-06-11 01:16:31 +0800

    Update main1.py

--- stitching_test/main1.py
+++ stitching_test/main1.py
@@ -5,10 +5,10 @@ for i in range(9):
     img.append(Image.open("rocket_{}_pred.png".format(i)))
 
 
-blendImg = Image.blend(img[0], img[1], alpha = 0.99)
+blendImg = Image.blend(img[0], img[1], alpha = 0.7)
 blendImg.save("output.png")
 
 output = Image.open("output.png")
 for i in range(2,9):
-    blendImg1 = Image.blend(output, img[i], alpha = 0.99)
+    blendImg1 = Image.blend(output, img[i], alpha = 0.7)
     blendImg1.save("output.png")
